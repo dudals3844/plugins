5cf3a60048a5385530e207256aef54c1a9b0c823
dudals3844 <44514383+dudals3844@users.noreply.github.com>
2020-08-27 18:49:06 +0900

    error solve

--- my_operators.py
+++ my_operators.py
@@ -7,16 +7,16 @@ from airflow.utils.decorators import apply_defaults
 log = logging.getLogger(__name__)
 
 class MyFirstOperator(BaseOperator):
+
     @apply_defaults
-    def __init__(self, my_operator_params, *args, **kwargs):
-        self.operator_param = my_operator_params
+    def __init__(self, my_operator_param, *args, **kwargs):
+        self.operator_param = my_operator_param
         super(MyFirstOperator, self).__init__(*args, **kwargs)
 
     def execute(self, context):
-        log.info("Hello World")
-        log.info("operator_param: %s", self.operator_param)
-
+        log.info("Hello World!")
+        log.info('operator_param: %s', self.operator_param)
 
 class MyFirstPlugin(AirflowPlugin):
-    name = 'my_first_plugin'
+    name = "my_first_plugin"
     operators = [MyFirstOperator]
