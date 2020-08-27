7ad8a6246f464ebc7f9a2706af2c2420cdd763c4
dudals3844 <44514383+dudals3844@users.noreply.github.com>
2020-08-27 15:00:55 +0900

    my_operators.py add

---
+++ my_operators.py
@@ -0,0 +1,22 @@
+import logging
+
+from airflow.models import BaseOperator
+from airflow.plugins_manager import AirflowPlugin
+from airflow.utils.decorators import apply_defaults
+
+log = logging.getLogger(__name__)
+
+class MyFirstOperator(BaseOperator):
+    @apply_defaults
+    def __init__(self, my_operator_params, *args, **kwargs):
+        self.operator_param = my_operator_params
+        super(MyFirstOperator, self).__init__(*args, **kwargs)
+
+    def excute(self, context):
+        log.info("Hello World")
+        log.info("operator_param: %s", self.operator_param)
+
+
+class MyFirstPlugin(AirflowPlugin):
+    name = 'my_first_plugin'
+    operators = [M
